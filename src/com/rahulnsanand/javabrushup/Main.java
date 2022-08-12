cdeb76f8b0a2d3e465c102e6fdc29d426e6e7531
rahulnsanand <theflopguymail@gmail.com>
2022-08-12 13:22:33 +0530

    Initial commit

---
+++ src/com/rahulnsanand/javabrushup/Main.java
@@ -0,0 +1,11 @@
+package com.rahulnsanand.javabrushup;
+
+import com.rahulnsanand.javabrushup.fundamentals.HelloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HelloWorld.print();
+    }
+
+}
---
+++ src/com/rahulnsanand/javabrushup/fundamentals/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.rahulnsanand.javabrushup.fundamentals;
+
+public class HelloWorld {
+
+   public static void print(){
+       System.out.println("Hello World");
+   }
+
+}
