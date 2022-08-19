986b6c9c58401941e875fe7c706bfb78cc8ec256
rahulnsanand <theflopguymail@gmail.com>
2022-08-19 14:13:32 +0530

    Added ReverseNumberPattern

--- src/com/rahulnsanand/javabrushup/pattern/ReverseNumberPattern.java
+++ src/com/rahulnsanand/javabrushup/pattern/ReverseNumberPattern.java
@@ -1,4 +1,20 @@
 package com.rahulnsanand.javabrushup.pattern;
 
+import java.util.Scanner;
+
 public class ReverseNumberPattern {
+    public static void run(){
+        Scanner scanner=new Scanner (System.in);
+        int n = scanner.nextInt();
+        int i = 1;
+        while(i<=n){
+            int j=i;
+            while(j>0){
+                System.out.print(j);
+                j -= 1;
+            }
+            System.out.println();
+            i += 1;
+        }
+    }
 }
