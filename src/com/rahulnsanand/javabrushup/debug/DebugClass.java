942328682324d023854c905259792ae24e6343cc
rahulnsanand <theflopguymail@gmail.com>
2022-08-22 16:35:40 +0530

    Added BitwiseOperator

--- src/com/rahulnsanand/javabrushup/debug/DebugClass.java
+++ src/com/rahulnsanand/javabrushup/debug/DebugClass.java
@@ -5,15 +5,8 @@ import java.util.Scanner;
 public class DebugClass {
 
     public static void debugThis(){
-        int a=10,b=20;
-        int c=a&b;
-        System.out.print(c);
-        int d=a|b;
-        System.out.print(d);
-        int e=a^b;
-        System.out.print(e);
-        int f=c+d+e;
-        System.out.print(~f);
+        int a=10,b=-20;
+        System.out.print(a^b);
     }
 
 }
