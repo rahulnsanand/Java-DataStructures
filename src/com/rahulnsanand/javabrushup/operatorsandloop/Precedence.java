41fae69c5325136724804ff0dfcf955cb55c346d
rahulnsanand <theflopguymail@gmail.com>
2022-08-22 18:32:48 +0530

    Added Precedence.java

---
+++ src/com/rahulnsanand/javabrushup/operatorsandloop/Precedence.java
@@ -0,0 +1,15 @@
+package com.rahulnsanand.javabrushup.operatorsandloop;
+
+public class Precedence {
+    public static void run(){
+        // Precedence Order
+        // Post Fix
+        // Unary (Prefix (++), Not (!), Or (~))
+        // Multiplicative
+        // Additive
+        // Shift (<<, >>, >>>)
+        // Relational (<, >, <=, >=, instanceof)
+        // Equality (==, !=)
+        // BitwiseAnd
+    }
+}
