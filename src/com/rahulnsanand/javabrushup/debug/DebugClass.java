c6d0290c9d524420419cdf10eac847ce3663cb6d
rahulnsanand <theflopguymail@gmail.com>
2022-08-30 18:10:18 +0530

    Updated DebugClass

--- src/com/rahulnsanand/javabrushup/debug/DebugClass.java
+++ src/com/rahulnsanand/javabrushup/debug/DebugClass.java
@@ -7,14 +7,17 @@ public class DebugClass {
     public static void debugThis(){
 
         int arr[] = new int[5];
-        arr[1] = 10;
-        arr = new int[2];
-        System.out.println(arr.length);
+        arr=change(arr);
+        System.out.println(arr[0]);
 
         System.out.println();
     }
 
-
+    public static int[] change(int input[]){
+        input = new int[5];
+        input[0] = 15;
+        return input;
+    }
     public static boolean isPrime(int x)
     {
         for(int i=2;i<x/2;i++)
