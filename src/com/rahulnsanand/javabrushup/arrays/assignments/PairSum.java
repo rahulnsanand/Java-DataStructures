e150f4acf4a4397660de767b381854dfa950098e
rahulnsanand <theflopguymail@gmail.com>
2022-08-30 19:57:08 +0530

    Added PairOfSum

---
+++ src/com/rahulnsanand/javabrushup/arrays/assignments/PairSum.java
@@ -0,0 +1,54 @@
+package com.rahulnsanand.javabrushup.arrays.assignments;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PairSum {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static int[] takeInput() throws IOException {
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] input = new int[size];
+
+        if (size == 0) {
+            return input;
+        }
+
+        String[] strNums;
+        strNums = br.readLine().split("\\s");
+
+
+        for (int i = 0; i < size; ++i) {
+            input[i] = Integer.parseInt(strNums[i]);
+        }
+
+        return input;
+    }
+
+
+    public static void run() throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while(t > 0) {
+
+            int[] input = takeInput();
+            int x = Integer.parseInt(br.readLine().trim());
+            System.out.println(pairSum(input, x));
+
+            t -= 1;
+        }
+    }
+
+    public static int pairSum(int arr[], int x) {
+        //Your code goes here
+        int outputCount = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]+arr[j]==x){
+                    outputCount++;
+                }
+            }
+        }
+        return outputCount;
+    }
+}
