9bb735247b7368590f5929758be1e6ee1c1298df
rahulnsanand <theflopguymail@gmail.com>
2022-08-31 19:08:55 +0530

    Added PushZeroToEnd.java

--- src/com/rahulnsanand/javabrushup/arrays/assignments/PushZeroToEnd.java
+++ src/com/rahulnsanand/javabrushup/arrays/assignments/PushZeroToEnd.java
@@ -47,15 +47,13 @@ public class PushZeroToEnd {
         }
     }
     public static void pushZerosAtEnd(int[] arr) {
-        int size = arr.length, temp;
-        for (int i = 0; i < size - 1; i++){
-            for (int j = 0; j < size - i - 1; j++){
-                if (arr[j] == 0) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
+        int count = 0, size = arr.length;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] != 0)
+                arr[count++] = arr[i];
+        }
+        while (count < size) {
+            arr[count++] = 0;
         }
     }
 }
