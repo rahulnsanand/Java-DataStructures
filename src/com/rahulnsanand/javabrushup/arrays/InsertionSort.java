82ea4a595f36a3ae21165aa3e687aaa687ed8d26
rahulnsanand <theflopguymail@gmail.com>
2022-08-31 18:57:36 +0530

    Added InsertionSort.java

---
+++ src/com/rahulnsanand/javabrushup/arrays/InsertionSort.java
@@ -0,0 +1,62 @@
+package com.rahulnsanand.javabrushup.arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InsertionSort {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static int[] takeInput() throws IOException {
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] input = new int[size];
+
+        if (size == 0) {
+            return input;
+        }
+
+        String[] strNums;
+        strNums = br.readLine().split("\\s");
+
+
+        for (int i = 0; i < size; ++i) {
+            input[i] = Integer.parseInt(strNums[i]);
+        }
+
+        return input;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while(t > 0) {
+
+            int[] input = takeInput();
+            insertionSort(input);
+            printArray(input);
+
+            t -= 1;
+        }
+    }
+    public static void insertionSort(int[] arr) {
+        int size = arr.length;
+        for (int i = 1; i < size; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+}
