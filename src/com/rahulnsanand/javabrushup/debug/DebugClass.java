dd15acc774cb1cb63b77d57bf3ca27ad9b8ca39a
rahulnsanand <theflopguymail@gmail.com>
2022-09-07 19:27:22 +0530

    Added LargestRowSum.java

--- src/com/rahulnsanand/javabrushup/debug/DebugClass.java
+++ src/com/rahulnsanand/javabrushup/debug/DebugClass.java
@@ -6,13 +6,30 @@ public class DebugClass {
 
     public static void debugThis(){
 
-        int[][] arr = { {1,2,4,5,7},{3,4,5,6,7},{5,6,7,8,9} };
-        System.out.println(arr.length+arr[0].length);
+        int[][] arr = new int[2][2];
+        fun(arr);
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                System.out.print(arr[i][j]+" " );
+            }
+        }
 
 
         System.out.println();
     }
+    public static void  fun(int[][] arr2d)
+    {
 
+        for(int i=0;i<arr2d.length;i++)
+        {
+            for(int j=0;j<arr2d[i].length;j++)
+            {
+                arr2d[i][j]=2*i+j;
+            }
+        }
+    }
     public static int[] change(int input[]){
         input = new int[5];
         input[0] = 15;
