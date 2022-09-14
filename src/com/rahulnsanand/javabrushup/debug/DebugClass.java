37b33f9701d405c44864d307d552e47a16df5bff
rahulnsanand <theflopguymail@gmail.com>
2022-09-14 18:27:05 +0530

    Added RectangularNumbers.java

--- src/com/rahulnsanand/javabrushup/debug/DebugClass.java
+++ src/com/rahulnsanand/javabrushup/debug/DebugClass.java
@@ -6,12 +6,29 @@ public class DebugClass {
 
     public static void debugThis(){
 
-        int num = 3;
-        print(num);
-
 
         System.out.println();
     }
+
+    public static String removeConsecutiveDuplicates(String s) {
+        // Write your code here
+        //aaab
+
+        if(s.length()==0){
+            return s;
+        }
+        int startIndex = 0, endIndex = 1, count = 0;
+        while(s.charAt(startIndex)==s.charAt(endIndex)){
+            endIndex++;
+            count++;
+        }
+        String outputValue = removeConsecutiveDuplicates(s.substring(count+1));
+        outputValue = s.charAt(0) + outputValue;
+        return outputValue;
+    }
+
+
+
     public static void print(int n){
         if(n < 0){
             return;
