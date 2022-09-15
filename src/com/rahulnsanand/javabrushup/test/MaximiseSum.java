73589778b64c1e4d2960961f34b9cefadd2d609d
rahulnsanand <theflopguymail@gmail.com>
2022-09-15 16:44:37 +0530

    Added MaximiseSum.java

--- src/com/rahulnsanand/javabrushup/test/MaximiseSum.java
+++ src/com/rahulnsanand/javabrushup/test/MaximiseSum.java
@@ -3,6 +3,7 @@ package com.rahulnsanand.javabrushup.test;
 import java.util.Scanner;
 
 public class MaximiseSum {
+    static long max(long x, long y) { return (x > y) ? x : y; }
     public static long maximumSumPath(int[] input1, int[] input2) {
         /* Your class should be named Solution
          * Don't write main().
@@ -10,40 +11,35 @@ public class MaximiseSum {
          * Return output and don't print it.
          * Taking input and printing output is handled automatically.
          */
+        int m = input1.length, n = input2.length;
 
-        int i = 0, j = 0;
-        int currSumArr1 = 0, currSumArr2 = 0, maximumSum = 0;
+        long maxsum=0;
+        long s1=0,s2=0;
+        int i=0,j=0;
 
-        while (i < input1.length && j < input2.length){
-            if (input1[i] < input2[j]){
-                currSumArr1 += input1[i];
+        while(i<m && j<n){
+            if(input1[i]<input2[j]){
+                s1+=input1[i];
                 i++;
-            } else if (input1[i] > input2[j]){
-                currSumArr2 += input2[j];
-
+            }
+            else if(input1[i]>input2[j]){
+                s2+=input2[j];
                 j++;
-            } else {
-                maximumSum += Math.max(currSumArr1, currSumArr2);
-                maximumSum += input1[i];
-                currSumArr1 = 0;
-                currSumArr2 = 0;
+            }
+            else{
+                maxsum+=max(s1,s2)+input1[i];
+                s1=0;
+                s2=0;
                 i++;
                 j++;
             }
         }
-
-        while (i < input1.length){
-            currSumArr1 += input1[i];
-            i++;
-        }
-
-        while (j < input2.length){
-            currSumArr2 += input2[j];
-            j++;
-        }
-        maximumSum += Math.max(currSumArr1, currSumArr2);
-
-        return maximumSum;
+        while (i<m)
+            s1+=input1[i++];
+        while (j<n)
+            s2+=input2[j++];
+        maxsum+=max(s1,s2);
+        return maxsum;
     }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
