3bc6b27b6a67d96d6335f660c21407da6e2aabba
rahulnsanand <theflopguymail@gmail.com>
2022-09-15 16:10:49 +0530

    Added MinimumLengthWord.java

---
+++ src/com/rahulnsanand/javabrushup/test/MinimumLengthWord.java
@@ -0,0 +1,25 @@
+package com.rahulnsanand.javabrushup.test;
+
+import java.util.Scanner;
+
+public class MinimumLengthWord {
+    public static String minLengthWord(String input){
+
+        String[] words = input.split(" ");
+        String shortestWord = words[0];
+        for (int i = 1; i < words.length; i++){
+            if(words[i].length()<shortestWord.length()){
+                shortestWord = words[i];
+            }
+        }
+
+        return shortestWord;
+
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+
+        System.out.println(minLengthWord(str));
+    }
+}
