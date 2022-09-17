0e9b3aa980ff8a5d276492c0eb778c8123c0099f
rahulnsanand <theflopguymail@gmail.com>
2022-09-17 19:47:11 +0530

    Added CheckAB.java

--- src/com/rahulnsanand/javabrushup/recursion/assignments/CheckAB.java
+++ src/com/rahulnsanand/javabrushup/recursion/assignments/CheckAB.java
@@ -1,4 +1,26 @@
 package com.rahulnsanand.javabrushup.recursion.assignments;
+import java.util.Scanner;
 
 public class CheckAB {
+    public static boolean checkAB(String input) {
+        // Write your code here
+        if(input.length() == 0){
+            return true;
+        }
+
+        if(input.charAt(0) == 'a'){
+            if(input.substring(1).length() > 1 && input.startsWith("bb", 1)){
+                return checkAB(input.substring(3));
+            }else{
+                return checkAB(input.substring(1));
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        System.out.println(checkAB(input));
+    }
 }
