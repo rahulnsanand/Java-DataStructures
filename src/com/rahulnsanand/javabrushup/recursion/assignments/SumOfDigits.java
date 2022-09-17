d2086360a9183fe4ce95f14fbc709c609ee3c4c9
rahulnsanand <theflopguymail@gmail.com>
2022-09-17 18:39:19 +0530

    Added SumOfDigits.java

--- src/com/rahulnsanand/javabrushup/recursion/assignments/SumOfDigits.java
+++ src/com/rahulnsanand/javabrushup/recursion/assignments/SumOfDigits.java
@@ -1,4 +1,19 @@
 package com.rahulnsanand.javabrushup.recursion.assignments;
 
+import java.util.Scanner;
+
 public class SumOfDigits {
+    public static int sumOfDigits(int input){
+        // Write your code here
+        if(input/10==0){
+            return input;
+        }
+
+        return input%10 + sumOfDigits(input/10);
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(sumOfDigits(n));
+    }
 }
